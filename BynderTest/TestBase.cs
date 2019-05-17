f7a2df6e7d7afc145303e07aab056dde400dfab5
Tobias Månsson <tobias.mansson@avensia.com>
2019-05-17 15:31:39 +0200

    Avensia Test function

--- BynderTest/TestBase.cs
+++ BynderTest/TestBase.cs
@@ -15,11 +15,11 @@ namespace BynderTest
 
         protected Dictionary<string,string> TestSettings = new Dictionary<string, string>
         {
-            {Bynder.Api.SettingNames.ConsumerKey, "***" },
-            {Bynder.Api.SettingNames.ConsumerSecret, "***" },
-            {Bynder.Api.SettingNames.CustomerBynderUrl, "***.getbynder.com" },
-            {Bynder.Api.SettingNames.Token, "***" },
-            {Bynder.Api.SettingNames.TokenSecret, "***" },
+            {Bynder.Api.SettingNames.ConsumerKey, "FF593D1D-5F1A-4449-88280AF539EE9D99" },
+            {Bynder.Api.SettingNames.ConsumerSecret, "7300085642fb8ce1443f0f27147c1382" },
+            {Bynder.Api.SettingNames.CustomerBynderUrl, "https://sigvaris.getbynder.com" },
+            {Bynder.Api.SettingNames.Token, "24DAECED-6996-4C23-99DB29C8EB527275" },
+            {Bynder.Api.SettingNames.TokenSecret, "729b22c258f0349587bdfcac140b97b2" },
             {Bynder.Config.Settings.RegularExpressionForFileName, @"^(?<ProductNumber>[0-9a-zA-Z]+)_(?<ResourceType>image|document)_(?<ResourcePosition>[0-9]+)" },
             {Bynder.Config.Settings.InitialAssetLoadUrlQuery, @"type=image" },
             {Bynder.Config.Settings.MetapropertyMap, @"C7BC01E1-670D-4410-A7B81E9032FE261A=ResourcePosition,C284234B-29B6-4CA8-B907B728455F30EA=ProductNumber" }
@@ -31,8 +31,8 @@ namespace BynderTest
             Logger = new Logger(TestContext);
             Logger.Log(LogLevel.Information, $"Initialize connection to inRiver Server");
             InRiverContext = new inRiverContext(
-                RemoteManager.CreateInstance("https://partner.remoting.productmarketingcloud.com",
-                    "***", "***"), Logger);
+                RemoteManager.CreateInstance("https://remoting.productmarketingcloud.com",
+                    "sigvaris@avensia.com", "Abcd!234", "Test"), Logger);
 
             Assert.IsNotNull(InRiverContext, "Login failed ??");
         }
