9f8e8ad6ac6d2bfa4ad342f873b73a0a34e0fbdb
patsza <patryk.szachagluchowicz@avensia.com>
2019-10-02 01:49:04 +0200

    Add ability to set a time on extension setting so that the scheduledExtension can run at given time once a day

--- BynderExtension/Extension/AssetLoader.cs
+++ BynderExtension/Extension/AssetLoader.cs
@@ -1,7 +1,16 @@
-﻿using Bynder.Api;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Bynder.Api;
 using Bynder.Workers;
+using inRiver.Remoting;
+using inRiver.Remoting.Connect;
 using inRiver.Remoting.Extension.Interface;
 using inRiver.Remoting.Log;
+using inRiver.Remoting.Objects;
+using inRiver.Remoting.Util;
 
 namespace Bynder.Extension
 {
@@ -14,11 +23,14 @@ namespace Bynder.Extension
         /// </summary>
         public void Execute(bool force)
         {
-            // only run manually
-            if (!force) return;
+            if (!force)
+            {
+                if (!ScheduledTime()) return;
+            }
 
             try
             {
+
                 Context.Logger.Log(LogLevel.Information, "Start loading assets");
 
                 var worker = Container.GetInstance<AssetUpdatedWorker>();
@@ -43,11 +55,75 @@ namespace Bynder.Extension
                     Context.Logger.Log(LogLevel.Information, $"Processed {counter} assets.");
                 }
                 Context.Logger.Log(LogLevel.Information, "Initial Import Successful!");
+
+                SaveConnectorState(DateTime.Now);
             }
             catch (System.Exception ex)
             {
                 Context.Log(LogLevel.Error, ex.GetBaseException().Message, ex);
             }
         }
+
+        private bool ScheduledTime()
+        {
+            var currentDateTime = DateTime.Now;
+            var connectorStates = Context.ExtensionManager.UtilityService.GetAllConnectorStatesForConnector(Context.ExtensionId);
+            if (connectorStates.Count.Equals(0))
+            {
+                var newConnectorState = new ConnectorState
+                {
+                    ConnectorId = Context.ExtensionId,
+                    Data = currentDateTime.ToString("g")
+                };
+                Context.ExtensionManager.UtilityService.AddConnectorState(newConnectorState);
+                return true;
+            }
+
+            var connectorState = connectorStates.Count > 1 ? connectorStates.OrderBy(state => state.Created).Last() : connectorStates.Single();
+
+            var lastRunTime = connectorState.Data;
+            if (string.IsNullOrEmpty(lastRunTime)) return false;
+
+
+            var (hours, minutes) = GetHoursAndMinutes();
+
+            if (currentDateTime.Hour.Equals(hours) && currentDateTime.Minute >= minutes)
+            {
+                var oneDayTimeSpan = new TimeSpan(24, 0, 0);
+                if (currentDateTime.Subtract(oneDayTimeSpan) >= DateTime.Parse(lastRunTime))
+                {
+                    DeleteConnectorStates(connectorStates);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void SaveConnectorState(DateTime dateTime)
+        {
+            var connectorState = new ConnectorState
+            {
+                ConnectorId = Context.ExtensionId,
+                Data = dateTime.ToString("g")
+            };
+            Context.ExtensionManager.UtilityService.AddConnectorState(connectorState);
+        }
+
+        private void DeleteConnectorStates(List<ConnectorState> connectorStates)
+        {
+            Context.ExtensionManager.UtilityService.DeleteConnectorStates(connectorStates.Select(c => c.Id).ToList());
+        }
+
+        private (int, int) GetHoursAndMinutes()
+        {
+            var time = ScheduledRun.Split(':');
+            if (int.TryParse(time[0], out var hours) && int.TryParse(time[1], out var minutes))
+            {
+                return (hours, minutes);
+            }
+            return (6, 0);
+        }
+
+        private string ScheduledRun => Context.Settings[Config.Settings.ScheduledTime];
     }
 }
